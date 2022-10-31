c03394765a4aff21c0047a54efe40968690cf278
RyanByrne <ryanbyrne68@gmail.com>
2022-10-31 10:48:56 +0200

    Optiized for Google Ads

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html","./node_modules/flowbite/**/*.js"],
+  content: [
+    "./node_modules/flowbite/**/*.js"
+],
   theme: {
     extend: {},
   },
-    plugins: [require("daisyui")],
+  plugins: [
+    require('flowbite/plugin')
+]
 }
+
 tailwind.config = {
   darkMode: 'class',
   theme: {
