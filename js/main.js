88c98743f228f0aebe37838588d28d04051cbac0
RyanByrne <ryanbyrne68@gmail.com>
2022-11-07 15:07:19 +0200

    Tweaks to CTAs and wording

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ var input = document.getElementById("main-input")
 function highlightInput() {
     input.focus();
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('defaultModalButton').click();
+});
